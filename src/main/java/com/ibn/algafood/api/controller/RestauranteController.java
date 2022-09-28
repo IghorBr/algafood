b7b66f51ebeefe9baf815b5bd6752ca122dd6ef5
IghorBr <ighorbruno20@gmail.com>
2022-09-28 17:08:41 -0300

    implementando verbo PATCH

--- src/main/java/com/ibn/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/ibn/algafood/api/controller/RestauranteController.java
@@ -1,5 +1,6 @@
 package com.ibn.algafood.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibn.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.ibn.algafood.domain.model.Restaurante;
 import com.ibn.algafood.domain.service.RestauranteService;
@@ -7,9 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -70,4 +74,34 @@ public class RestauranteController {
         }
     }
 
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> partialUpdate(@PathVariable("id") Long id, @RequestBody Map<String, Object> fields) {
+        try {
+            Restaurante restaurante = retornaRestaurantePreenchido(id, fields);
+            restaurante = restauranteService.save(restaurante);
+
+            return ResponseEntity.ok(restaurante);
+        } catch (IllegalAccessException | IllegalArgumentException e ) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    private Restaurante retornaRestaurantePreenchido(final Long id, Map<String, Object> fields) throws IllegalAccessException {
+        Restaurante restaurante = restauranteService.findById(id).orElseThrow(() -> new EntidadeNaoEncontradaException(String.format("Restaurante de código %s não encontrado.", id)));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Restaurante novoRestaurante = objectMapper.convertValue(fields, Restaurante.class);
+
+        for (String key : fields.keySet()) {
+            Field field = ReflectionUtils.findField(Restaurante.class, key);
+            field.setAccessible(true);
+
+            Object obj = field.get(novoRestaurante);
+            ReflectionUtils.setField(field, restaurante, obj);
+        }
+
+        return restaurante;
+    }
 }
