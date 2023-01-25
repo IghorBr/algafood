8d76172b61408216cf101b641ed60724ff482f28
IghorBr <ighorbruno20@gmail.com>
2023-01-25 10:53:26 -0300

    desafio 1 cache HTTP

--- src/main/java/com/ibn/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/ibn/algafood/api/controller/FormaPagamentoController.java
@@ -7,12 +7,14 @@ import com.ibn.algafood.core.validation.Groups;
 import com.ibn.algafood.domain.model.FormaPagamento;
 import com.ibn.algafood.domain.service.FormaPagamentoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/formas-pagamento")
@@ -24,20 +26,16 @@ public class FormaPagamentoController {
 
     @GetMapping
     public ResponseEntity<List<FormaPagamentoOutDTO>> findAll() {
-        return ResponseEntity.ok(
-                formaPagamentoMapper.domainListToDto(
-                        formaPagamentoService.findAll()
-                )
-        );
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+                .body(formaPagamentoMapper.domainListToDto(formaPagamentoService.findAll()));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<FormaPagamentoOutDTO> findById(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(
-                formaPagamentoMapper.domainToDto(
-                        formaPagamentoService.findById(id)
-                )
-        );
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(20, TimeUnit.SECONDS))
+                .body(formaPagamentoMapper.domainToDto(formaPagamentoService.findById(id)));
     }
 
     @PostMapping
